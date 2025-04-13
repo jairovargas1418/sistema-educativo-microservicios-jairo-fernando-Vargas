056494f9039228461408af1d4b5acfb0b7388f5b
Jairo Fernando Vargas Pimiento <94024758+jairovargas1418@users.noreply.github.com>
2025-04-13 09:23:20 -0500

    Delete matriculas-servicio directory

--- matriculas-servicio/src/main/java/com/asignaturas/servicio/repositor/MatriculaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.asignaturas.servicio.repositor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.matriculas.servicio.model.Matricula;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Integer> {
-}
--- matriculas-servicio/src/main/java/com/matriculas/servicio/controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.matriculas.servicio.controller;
