876742f4005fdc879c85ade4602f00f9e4d236ae
Jairo Fernando Vargas Pimiento <94024758+jairovargas1418@users.noreply.github.com>
2025-04-21 07:54:43 -0500

    Delete usuarios-servicio directory

--- usuarios-servicio/src/main/java/org/springframework/security/config/annotation/web/configuration/WebSecurityConfigurerAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.security.config.annotation.web.configuration;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-public @interface WebSecurityConfigurerAdapter {
-
-	void configure(HttpSecurity http) throws Exception;
-
-}
